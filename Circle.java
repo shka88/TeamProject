af782817b4684779d75be2d9f70dd56a6b788d0a
jiyeon <gbko159@naver.com>
2019-09-03 16:00:16 +0900

    Circle 이지연

--- Circle.java
+++ Circle.java
@@ -7,5 +7,15 @@
  */
 public class Circle implements Shape
 {
+    int radius;
     
+    public Circle(int radius){
+        this.radius=radius;
+    }
+    public void draw(){
+        System.out.println("반지름이 "+radius+"인 원입니다.");
+    }
+    public double getArea(){
+        return radius*radius*PI;
+    }
 }
