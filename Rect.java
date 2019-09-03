285190707ecc303f629c01c5fb3bd4fe00f6df69
YangYousuk <yys9905@naver.com>
2019-09-03 09:36:07 +0900

    add Rect

--- Rect.java
+++ Rect.java
@@ -7,5 +7,19 @@
  */
 public class Rect implements Shape
 {
+    int width; 
+    int height;
     
+    public Rect(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+ 
+    public void draw(){
+        System.out.println(width + "x" + height +"크기의 사각형입니다.");
+    }
+    
+    public double getArea(){
+        return width*height;
+    }
 }
