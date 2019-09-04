9c5b174c2688aa6acb05281d4701fcc70776921c
LEE GA YOUNG <lky1732@gmail..com>
2019-09-04 14:39:52 +0900

    학번 이름 작성

--- Oval.java
+++ Oval.java
@@ -2,7 +2,7 @@
 /**
  * Write a description of class Oval here.
  *
- * @author (your name)
+ * @author (2018315036 양유석, 2018315030 이가영, 2018315022 이지연)
  * @version (2019.09.04)
  */
 public class Oval implements Shape
