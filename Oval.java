6814262b94967959adf091444a5f25b0b254300c
LEE GA YOUNG <lky1732@gmail.com>
2019-09-04 14:59:46 +0900

    oval class 설명

--- Oval.java
+++ Oval.java
@@ -1,13 +1,13 @@
 
 /**
- * Write a description of class Oval here.
- *
+ * Oval의 장축과 단축을 이용하여 타원의 면적을 구하는 class
+ * 
  * @author (2018315036 양유석, 2018315030 이가영, 2018315022 이지연)
  * @version (2019.09.04)
  */
 public class Oval implements Shape
 {
-    int width; 
+    int width;    
     int height;
     
     public Oval(int width, int height){
